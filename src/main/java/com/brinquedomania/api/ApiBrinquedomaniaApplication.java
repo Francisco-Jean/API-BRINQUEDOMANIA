05bdbdc785dd6a1a0a57b4be5a6ae46db90e51c3
Francisco Jean <71938841+Francisco-Jean@users.noreply.github.com>
2023-06-09 23:30:02 -0300

    Start API

---
+++ src/main/java/com/brinquedomania/api/ApiBrinquedomaniaApplication.java
@@ -0,0 +1,13 @@
+package com.brinquedomania.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBrinquedomaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBrinquedomaniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brinquedomania/api/ApiBrinquedomaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brinquedomania.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBrinquedomaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
