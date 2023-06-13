75bfe843883bb57ee27196f2ac66622eeb0d2ad8
gustavoib <joaogustavo831@gmail.com>
2023-06-13 17:34:25 -0300

    implementação post e get - controller

--- src/main/java/com/brinquedomania/api/controllers/ProductController.java
+++ src/main/java/com/brinquedomania/api/controllers/ProductController.java
@@ -4,9 +4,15 @@ import com.brinquedomania.api.dtos.UserRecordDto;
 import com.brinquedomania.api.models.UserModel;
 import com.brinquedomania.api.repositories.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -14,14 +20,29 @@ public class ProductController {
     @Autowired
     UserRepository userRepository;
 
+    @PostMapping("/user/cadastro")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDto, userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(userModel));
+    }
+    @GetMapping("/user/cadastro")
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
+    }
+    @GetMapping("/user/cadastro/{id}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "id") UUID id) {
+        Optional<UserModel> user0 = userRepository.findById(id);
+        if (user0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("usuário não encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(user0.get());
+    }
     /*
-    @PostMapping("/products")
-    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto)
-
-    */
-
     @GetMapping("/home/{name}")
     public String bemVindo(@PathVariable String name){
         return "Bem-vindo, " + name;
     }
+    */
 }
