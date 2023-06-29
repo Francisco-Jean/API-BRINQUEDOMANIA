e2d9ec8e0b90dc2158cd4fe902a727d227428f06
gcirineu18 <guilhermecirineu@hotmail.com>
2023-06-28 23:23:38 -0300

    Validação cadastro de produtos

--- src/app/private/seller/seller.component.ts
+++ src/app/private/seller/seller.component.ts
@@ -5,7 +5,7 @@ import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { LoginService } from './../../components/login/login.service';
 import { catchError,tap, throwError } from 'rxjs';
-import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms'
 import { error } from 'console';
 
 
@@ -22,26 +22,37 @@ export class SellerComponent {
   public value:string="";
   public description: string="";
   public id: string | null;
-  public imageLink: string=""
+ public imageLink: any;
 
   constructor(private http: HttpClient, private toast: ToastrService, private route: Router, private fb: FormBuilder, private loginService:LoginService){
     this.formCadastroProduto = this.criaFormCadastro()
 
     const {name, type, id} = this.loginService.getData();
     this.id = id
+
+    this.imageLink = null
   }
-  
+
+  // Irá lidar com o evento da seleção do arquivo e irá
+  // estabelecer o arquivo no form control:
+     selectImage(event: any){
+  this.imageLink = event?.target.files[0]
+}
   public criaFormCadastro(): FormGroup{
     return this.fb.group({
-     name:["",[Validators.required,  ]],
+     name:["",[Validators.required, Validators.minLength(3) ]],
      category:["",[Validators.required,  ]],
-     value:["",[Validators.required,  ]],
-     description:["",[Validators.required,  ]],
+     value:["",[Validators.required, Validators.pattern(/^\d{1,5}.\d{2}$/)  ]],
+     description:["",[Validators.required, Validators.minLength(10) ]],
      imageLink:["",[Validators.required,  ]],
     })
   }
 
+
+
   public register(){
+    const formData: FormData = new FormData
+    formData.append('imageLink', this.imageLink)
     
     const url = `${environment.baseUrlBackend}/product/register`;
     let bodyData ={
@@ -50,7 +61,7 @@ export class SellerComponent {
       "value": this.value,
       "description": this.description,
       "idSeller": this.id,
-      "imageLink":"https://external-content.duckduckgo.com"
+      "imageLink": formData
 
     }
 
